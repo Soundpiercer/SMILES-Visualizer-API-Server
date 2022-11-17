65da60b2bcfda6c6897fac5d13243db761e2f423
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-11-17 14:17:34 +0900

    Implement something related with user

--- mole/models.py
+++ mole/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Molecular(models.Model):
+    owner = models.ForeignKey(
+        "auth.User", related_name="molecular", on_delete=models.CASCADE
+    )
     created = models.DateTimeField(auto_now_add=True)
     formula = models.CharField(max_length=100, blank=True, default="")
     effect = models.CharField(max_length=500, blank=True, default="")
--- mole/serializers.py
+++ mole/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Molecular
+
 
 class MolecularSerializer(serializers.ModelSerializer):
     class Meta:
         model = Molecular
-        fields = ['id', 'formula', 'effect', 'others']
+        fields = ["id", "formula", "effect", "others"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    moleculars = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Molecular.objects.all()
+    )
+
+    class Meta:
+        model = User
+        field = ["id", "username", "moleculars"]
